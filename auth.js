6cdee4fb90db2d6a1ed736a2a0bce1d1b12536c4
Khakhack <floriandefaria@yahoo.fr>
2025-04-28 11:01:50 +0200

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,30 @@
+// Initialisation de l'authentification
+const auth = firebase.auth();
+
+// Fonction pour s'inscrire
+function inscrireUtilisateur(email, motdepasse) {
+    auth.createUserWithEmailAndPassword(email, motdepasse)
+        .then((userCredential) => {
+            console.log('Inscription réussie', userCredential.user);
+            alert("Inscription réussie !");
+            window.location.href = "index.html"; // Redirige vers l'accueil après inscription
+        })
+        .catch((error) => {
+            console.error('Erreur inscription', error.message);
+            alert(error.message);
+        });
+}
+
+// Fonction pour se connecter
+function connecterUtilisateur(email, motdepasse) {
+    auth.signInWithEmailAndPassword(email, motdepasse)
+        .then((userCredential) => {
+            console.log('Connexion réussie', userCredential.user);
+            alert("Connexion réussie !");
+            window.location.href = "index.html"; // Redirige vers l'accueil après connexion
+        })
+        .catch((error) => {
+            console.error('Erreur connexion', error.message);
+            alert(error.message);
+        });
+}
