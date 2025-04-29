e462632cd119b581264cea7b6a264fc26002b0d4
Khakhack <floriandefaria@yahoo.fr>
2025-04-29 07:08:48 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,22 +1,22 @@
-// Importer Firebase SDK
+// Import Firebase App et Firestore
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
-// Ta configuration Firebase
+// Configuration de ton app Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDMyuNMouCQEUlG6cg2uE2ib9cI-WWnx-o",
-    authDomain: "assmatsim.firebaseapp.com",
-    projectId: "assmatsim",
-    storageBucket: "assmatsim.firebasestorage.app",
-    messagingSenderId: "73332672940",
-    appId: "1:73332672940:web:29243e73f8f13195a4d209",
-    measurementId: "G-DYJD1PEXZ9"
+  apiKey: "AIzaSyDMyuNMouCQEUlG6cg2uE2ib9cI-WWnx-o",
+  authDomain: "assmatsim.firebaseapp.com",
+  projectId: "assmatsim",
+  storageBucket: "assmatsim.appspot.com", // corrigé ici : ".com" ajouté
+  messagingSenderId: "73332672940",
+  appId: "1:73332672940:web:29243e73f8f13195a4d209",
+  measurementId: "G-DYJD1PEXZ9"
 };
 
-// Initialisation de Firebase
+// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialisation de Firestore et Auth
+// Initialiser Firestore + Auth
 export const db = getFirestore(app);
 export const auth = getAuth(app);
